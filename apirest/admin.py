9eed7a5ee70f98c86549c3e081151f756410823b
frankmwilsonm <frank.mamani@upeu.edu.pe>
2021-11-03 15:09:14 -0500

    admin personalizado /gitu

--- apirest/admin.py
+++ apirest/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
 from .models import Proveedor
+
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('nombres', 'apellidos', 'razonsocial', 'ruc', 'celular')
 
 admin.site.register(Proveedor, ProveedorAdmin) #Quita ProductoAdmin si no funciona
 
