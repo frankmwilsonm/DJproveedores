fc45f83d7efc015da0256c1645c32068a819661d
frankmwilsonm <frank.mamani@upeu.edu.pe>
2021-11-03 15:15:12 -0500

    add serializers.py /gitu

---
+++ apirest/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Proveedor
+
+class ProveedorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model-Proveedor
+        fields = ('id', 'nombres', 'apellidos', 'razonsocial','direccion', 'ruc', 'celular')
