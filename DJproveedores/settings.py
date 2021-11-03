ec1c4780adf3014e22a6b5ef93c9c43b85ceeeff
frankmwilsonm <frank.mamani@upeu.edu.pe>
2021-11-03 16:17:33 -0500

    Deploy to HEROKU /gitu

--- DJproveedores/settings.py
+++ DJproveedores/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
