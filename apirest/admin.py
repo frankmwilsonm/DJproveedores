e1a3bf6d5afd4e8a0aecb1676da239d833a532ac
frankmwilsonm <frank.mamani@upeu.edu.pe>
2021-11-03 15:00:44 -0500

    modelo proveedor creado y registrado en admin /gitu

--- apirest/admin.py
+++ apirest/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Proveedor
+
+admin.site.register(Proveedor, ProveedorAdmin) #Quita ProductoAdmin si no funciona
+
 # Register your models here.
--- apirest/models.py
+++ apirest/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Proveedor(models.Model):
+        ##Atributos
+    nombres=models.CharField(max_length=50)
+    apellidos=models.CharField(max_length=50)
+    razonsocial=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=250)
+    ruc=models.CharField(max_length=50)
+    celular=models.CharField(max_length=15)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = ("Proveedor")
+        verbose_name_plural = ("Proveedores")
+
+    def __str__(self):
+        return self.nombres #name Cambia segun Atributo queramos regresar
+
+
 # Create your models here.
