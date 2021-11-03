2235bba72290f828e80317bb148ca570c0320a9f
frankmwilsonm <frank.mamani@upeu.edu.pe>
2021-11-03 15:18:11 -0500

    add vistas /gitu

--- apirest/views.py
+++ apirest/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .serializers import ProveedorSerializer
+from .models import Proveedor
+
+class ProveedorViewSet (viewsets.ModelViewSet):
+    queryset-Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+
 # Create your views here.
